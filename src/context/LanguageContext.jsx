1e75789bc801de7d37b75d2e53fdf84fc790a1c0
FatmaWafy <fatmaa1601@gmail.com>
2025-01-06 19:15:05 +0200

    lab-5

--- src/context/LanguageContext.jsx
+++ src/context/LanguageContext.jsx
@@ -1,16 +1,12 @@
 /* eslint-disable */
 import React, { createContext, useState, useContext } from "react";
-
-// إنشاء Context
 const LanguageContext = createContext();
-
-// مزود (Provider) لإدارة اللغة
 export const LanguageProvider = ({ children }) => {
   const [language, setLanguage] = useState("en");
 
   const changeLanguage = (lang) => {
     setLanguage(lang);
-    document.documentElement.dir = lang === "ar" ? "rtl" : "ltr"; // تعديل الاتجاه
+    document.documentElement.dir = lang === "ar" ? "rtl" : "ltr";
   };
 
   return (
@@ -19,6 +15,4 @@ export const LanguageProvider = ({ children }) => {
     </LanguageContext.Provider>
   );
 };
-
-// دالة لتسهيل الوصول إلى الـ Context
 export const useLanguage = () => useContext(LanguageContext);
